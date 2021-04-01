667b94f813fe7546c8cea813ff68a9d69dbb08c1
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2021-03-31 21:46:55 -0600

    Added RegularExpression.py

---
+++ RegularExpression.py
@@ -0,0 +1,48 @@
+def validateRE(rule, word):
+    patron = ""
+    stackLetters = []
+    currentState = True
+    reState = True
+    re = False
+    for i, char in enumerate(rule):
+        if char == "+":
+            re = True
+            if not (validateRE(rule[i + 2::], word)):
+                reState = False
+        elif char == "(":
+            for letter in stackLetters:
+                patron += letter
+            stackLetters = []
+        elif char == "*":
+            if not isOnTheWord(stackLetters, word):
+                currentState = False
+            else:
+                stackLetters = []
+        else:
+            if char != ")":
+                stackLetters.append(char)
+    if stackLetters:
+        for letter in stackLetters:
+            patron += letter
+
+    if patron != "" and len(patron) < len(word):
+        for i in range(len(patron)):
+            if word[i] != patron[i]:
+                currentState = False
+
+    if currentState:
+        return True
+    else:
+        if reState and re:
+            return True
+        else:
+            return False
+
+def isOnTheWord(pattern, word):
+    newPattern = ""
+    for letter in pattern:
+        newPattern += letter
+    if newPattern in word:
+        return True
+    else:
+        return False
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
+import RegularExpression as re
 
 path = 'C:/Users\estef\Documents\prueba'
-
 
 def getRE(rule, dir):
     files = getFilesNames(dir)
     finalPaths = []
     for file in files:
-        if validateRE(rule, file):
+        if re.validateRE(rule, file):
             newPath = searchFile(file, dir)
             if newPath:
                 for path in newPath:
@@ -24,60 +24,8 @@ def searchFile(word, path):
     return files if len(files) != 0 else False
 
 def getFilesNames(path):
-    f = []
+    filesNames = []
     for (dirpath, dirnames, filenames) in os.walk(path):
-        f.extend(filenames)
+        filesNames.extend(filenames)
         break
-    return f
-
-def validateRE(rule, word):
-    patron = ""
-    stackLetters = []
-    currentState = True
-    reState = True
-    re = False
-    for i, char in enumerate(rule):
-        if char == "+":
-            re = True
-            if not (validateRE(rule[i + 2::], word)):
-                reState = False
-        elif char == "(":
-            for letter in stackLetters:
-                patron += letter
-            stackLetters = []
-        elif char == "*":
-            if not isOnTheWord(stackLetters, word):
-                currentState = False
-            else:
-                stackLetters = []
-        else:
-            if char != ")":
-                stackLetters.append(char)
-    if stackLetters:
-        for letter in stackLetters:
-            patron += letter
-
-    if patron != "" and len(patron) < len(word):
-        for i in range(len(patron)):
-            if word[i] != patron[i]:
-                currentState = False
-
-    if currentState:
-        return True
-    else:
-        if reState and re:
-            return True
-        else:
-            return False
-
-def isOnTheWord(pattern, word):
-    newPattern = ""
-    for letter in pattern:
-        newPattern += letter
-    if newPattern in word:
-        return True
-    else:
-        return False
-
-
-print(getRE("ab*", path))
+    return filesNames
