91c409dcf714514cf43b969dbb57d79cf6aff9bd
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2021-03-31 21:30:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,20 @@ import os
 
 path = 'C:/Users\estef\Documents\prueba'
 
-def getRE(word,dir):
-    rule = re.compile(word)
+
+def getRE(rule, dir):
     files = getFilesNames(dir)
     finalPaths = []
     for file in files:
-        names = rule.findall(file)
-        for name in names:
-                newPath = searchFile(name,dir)
-                if newPath:
-                    for path in newPath:
-                        if path not in finalPaths:
-                            finalPaths.append(path)
+        if validateRE(rule, file):
+            newPath = searchFile(file, dir)
+            if newPath:
+                for path in newPath:
+                    if path not in finalPaths:
+                        finalPaths.append(path)
     return finalPaths
 
-def searchFile(word,path):
+def searchFile(word, path):
     files = []
     for i in os.listdir(path):
         if os.path.isfile(os.path.join(path, i)) and word in i:
@@ -32,4 +31,54 @@ def getFilesNames(path):
         break
     return f
 
-print(getRE('ab*',path))
+def validateRE(rule, word):
+    patron = ""
+    stackLetters = []
+    currentState = True
+    reState = True
+    re = False
+    for i, char in enumerate(rule):
+        if char == "+":
+            re = True
+            if not (validateRE(rule[i + 2::], word)):
+                reState = False
+        elif char == "(":
+            for letter in stackLetters:
+                patron += letter
+            stackLetters = []
+        elif char == "*":
+            if not isOnTheWord(stackLetters, word):
+                currentState = False
+            else:
+                stackLetters = []
+        else:
+            if char != ")":
+                stackLetters.append(char)
+    if stackLetters:
+        for letter in stackLetters:
+            patron += letter
+
+    if patron != "" and len(patron) < len(word):
+        for i in range(len(patron)):
+            if word[i] != patron[i]:
+                currentState = False
+
+    if currentState:
+        return True
+    else:
+        if reState and re:
+            return True
+        else:
+            return False
+
+def isOnTheWord(pattern, word):
+    newPattern = ""
+    for letter in pattern:
+        newPattern += letter
+    if newPattern in word:
+        return True
+    else:
+        return False
+
+
+print(getRE("ab*", path))
